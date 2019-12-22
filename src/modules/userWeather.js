7cedb8c6afbadfe15d70a8df161bfb2c0fa254cb
Elena Capienko <elena.capienko@gmail.com>
2019-12-22 10:30:43 -0500

    fix: bug

--- src/modules/userWeather.js
+++ src/modules/userWeather.js
@@ -4,7 +4,7 @@ export default userWeather;
 
 function userWeather(curLocation) {
   const WEATHER_API_TOKEN = '835b3e05313f23d5e8c08b3407ca00be';
-  return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${curLocation.latitude}&lon=${curLocation.longitude}&units=metric&APPID=${WEATHER_API_TOKEN}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${curLocation.latitude}&lon=${curLocation.longitude}&units=metric&APPID=${WEATHER_API_TOKEN}`)
     .then((response) => response.json())
     .then((data) => data);
 }
